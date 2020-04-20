4262318a5f2b9e7fdb0d2d3625418752325f1c1b
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-04-20 14:49:59 +0200

    Génération de la room

--- game/Kusinta/Room/game/room/Room.java
+++ game/Kusinta/Room/game/room/Room.java
@@ -19,57 +19,72 @@ public class Room {
 	InnerWallImageManager IWIM = new InnerWallImageManager(ambiance);
 	EmptySpaceImageManager ESIM = new EmptySpaceImageManager(ambiance);
 
-	public Room() throws IOException {
-		BufferedReader f = new BufferedReader(new FileReader(new File(roomFile)));
-		/*
-		 * Le fichier suis cette syntaxe: Row:Col CODE/CODE/CODE/...../ ... ... ...
-		 */
-		String[] firstLine = f.readLine().split(":");
-		nbRow = Integer.parseInt(firstLine[0]);
-		nbCol = Integer.parseInt(firstLine[1]);
+	public Room() {
+		BufferedReader f;
+		try {
+			roomFile = "resources/Room/Sample/room1.sample";
+			f = new BufferedReader(new FileReader(new File(roomFile)));
 
-		for (int i = 0; i < nbRow; i++) {
-			String[] actualLigne = f.readLine().split("/");
-			for (int j = 0; j < nbCol; j++) {
-				m_elements[i * nbCol + j] = CodeElement(actualLigne[j], j * nbCol * Element.SIZE,
-						i * nbRow * Element.SIZE);
+			/*
+			 * Le fichier suis cette syntaxe: 
+			 * Row:Col 
+			 * CODE/CODE/CODE/...../ 
+			 * ... 
+			 * ... 
+			 * ...
+			 */
+			String[] firstLine = f.readLine().split(":");
+			nbRow = Integer.parseInt(firstLine[0]);
+			nbCol = Integer.parseInt(firstLine[1]);
+			m_elements = new Element[nbRow * nbCol];
+			for (int i = 0; i < nbRow; i++) {
+				String[] actualLigne = f.readLine().split("/");
+				for (int j = 0; j < nbCol; j++) {
+					m_elements[i * nbCol + j] = CodeElement(actualLigne[j], j * nbCol * Element.SIZE,
+							i * nbRow * Element.SIZE);;
+				}
 			}
+			f.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		f.close();
 	}
 
-	public Element CodeElement(String code, int x, int y) throws IOException {
+	public Element CodeElement(String code, int x, int y) throws Exception {
 		Coord coord = new Coord(x, y);
-		if (code == "IW") {
+		if (code.equals("IW")) {
 			return new InnerWall(coord, IWIM);
-
-		} else if (code == "OW_E") {
+		} else if (code.equals("OW_E")) {
 			return new OuterWall(coord, OWIM, "E");
-		} else if (code == "OW_S") {
+		} else if (code.equals("OW_S")) {
 			return new OuterWall(coord, OWIM, "S");
-		} else if (code == "OW_W") {
+		} else if (code.equals("OW_W")) {
 			return new OuterWall(coord, OWIM, "W");
-		} else if (code == "OW_N") {
+		} else if (code.equals("OW_N")) {
 			return new OuterWall(coord, OWIM, "N");
-		} else if (code == "OW_SE") {
+		} else if (code.equals("OW_SE")) {
 			return new OuterWall(coord, OWIM, "SE");
-		} else if (code == "OW_SW") {
+		} else if (code.equals("OW_SW")) {
 			return new OuterWall(coord, OWIM, "SW");
-		} else if (code == "OW_NW") {
+		} else if (code.equals("OW_NW")) {
 			return new OuterWall(coord, OWIM, "NW");
-		} else if (code == "OW_NE") {
+		} else if (code.equals("OW_NE")) {
 			return new OuterWall(coord, OWIM, "NE");
-		} else if (code == "ES") {
+		} else if (code.equals("ES")) {
 			return new EmptySpace(coord, ESIM);
-		} else if (code == "ES_I") {
+		} else if (code.equals("ES_I")) {
 			return new EmptySpace(coord, ESIM);
+		} else {
+			System.out.println("Non reach point. Code Error on : " + code);
 		}
-		throw new IllegalArgumentException("Room: Code element");
+
+		throw new Exception("Code room err: " + code);
 
 	}
 
 	public void paint(Graphics g) {
-		for (int i = 0; i <m_elements.length; i ++) {
+		for (int i = 0; i < m_elements.length; i++) {
 			m_elements[i].paint(g);
 		}
 	}
