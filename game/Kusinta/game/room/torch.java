d1e403431e866d8cecfdfc302e15f5614108c7f4
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-04-21 16:00:36 +0200

    Ajout de torch

--- game/Kusinta/game/room/torch.java
+++ game/Kusinta/game/room/torch.java
@@ -6,11 +6,14 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import automaton.Category;
+import automaton.Direction;
+import game.Coord;
+
 public class torch extends Decor{
 
-	public torch(boolean isSolid, boolean isVisible) {
-		super(isSolid, isVisible);
-		isAnimated = true;
+	public torch(Coord coord) {
+		super(false, true, true, coord);
 		loadImages();
 	}
 	
@@ -22,8 +25,6 @@ public class torch extends Decor{
 		      m_imageElapsed = 0;
 		      m_imageIndex = (m_imageIndex + 1) % m_images.length;
 		      __image = m_images[m_imageIndex];
-		      getCoord().setX(getCoord().X() + (SIZE - __image.getWidth(null) / 2));
-		      getCoord().setY(getCoord().Y() + (SIZE - __image.getHeight(null) / 2));
 		    }
 		}
 	
@@ -32,13 +33,126 @@ public class torch extends Decor{
 	private void loadImages() {
 		m_images = new BufferedImage[4];
 		try {
+			m_imageIndex = 0;
 			m_images[0] = ImageIO.read(new File("resources/Room/decors/torch2_1.png"));
 			m_images[1] = ImageIO.read(new File("resources/Room/decors/torch2_2.png"));
 			m_images[2] = ImageIO.read(new File("resources/Room/decors/torch2_3.png"));
 			m_images[3] = ImageIO.read(new File("resources/Room/decors/torch2_4.png"));
+			__image = m_images[m_imageIndex];
+			getCoord().setX(getCoord().X() + (SIZE - __image.getWidth(null) / 2));
+		      getCoord().setY(getCoord().Y() + (SIZE - __image.getHeight(null) / 2));
 		} catch (IOException e) {
+			System.out.println("Erreur while longind torch animation");
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public boolean move(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean jump(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pop(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean wizz(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean power() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pick(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean turn(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean get() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean store() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean explode() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean egg(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hit(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mydir(Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean cell(Direction dir, Category cat) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean closest(Category cat, Direction dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean gotstuff() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean gotpower() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean key(int keyCode) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
