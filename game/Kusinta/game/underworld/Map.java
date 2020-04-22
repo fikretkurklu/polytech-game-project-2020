831b50f92202018a0e7a117953f233e0e4946a4e
Yaël <ya38able@gmail.com>
2020-04-22 11:37:02 +0200

    Ajout du dossier underworld

---
+++ game/Kusinta/game/underworld/Map.java
@@ -0,0 +1,18 @@
+package underworld;
+
+import java.awt.Graphics;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import game.Coord;
+import room.Element;
+
+public class Map {
+	
+	int m_width, m_height;
+	Element[] m_elements;
+	int ambiance = 1;
+
+}
