daf62865c2e47e4379f6a0d8be47d3f9e9d75a6c
Ethan Malecot <ethanmalecot.99@gmail.com>
2020-04-24 14:49:22 +0200

    Debut des equipements - initialisation de la branche

---
+++ game/Kusinta/game/equipement/Equipement.java
@@ -0,0 +1,9 @@
+package equipement;
+
+public abstract class Equipement {
+
+	public abstract void equiped();
+	
+	public abstract void unequiped();
+	
+}
---
+++ game/Kusinta/game/equipement/Leggings.java
@@ -0,0 +1,5 @@
+package equipement;
+
+public class Leggings {
+
+}
