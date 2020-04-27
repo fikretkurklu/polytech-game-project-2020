5f9b31ccf4bc02c01e1817979993b570bd1a974b
Tioye Toue <tioye.toue@etu.univ-grenoble-alpes.fr>
2020-04-27 17:18:59 +0200

    test map3

--- game/Kusinta/game/underworld/UnderworldParam.java
+++ game/Kusinta/game/underworld/UnderworldParam.java
@@ -1,7 +1,7 @@
 package underworld;
 
 public interface UnderworldParam {
-	String mapFile = "resources/Underworld/Sample/map4.sample";
+	String mapFile = "resources/Underworld/Sample/map3.sample";
 	int nbAmbiance = 1;
 	String[] cloudImage = {"resources/Underworld/Clouds/Cloud1.png", "resources/Underworld/Clouds/Cloud2.png", "resources/Underworld/Clouds/Cloud3.png"};
 	String[] playerSoulImage = {"resources/Underworld/PlayerSoul/dark-ghost-s-1.png", "resources/Underworld/PlayerSoul/dark-ghost-s-2.png", "resources/Underworld/PlayerSoul/dark-ghost-s-3.png", "resources/Underworld/PlayerSoul/dark-ghost-s-4.png"};
