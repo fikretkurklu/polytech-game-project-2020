52e605582735082a0ea6d7cbbd4dff8138c64c8d
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-05-09 16:52:05 +0200

    Modif HUD 1

--- game/Kusinta/game/hud/HUD.java
+++ game/Kusinta/game/hud/HUD.java
@@ -10,6 +10,7 @@ public class HUD {
 	int m_x, m_y, m_width, m_height;
 	HUDFont m_font;
 	HUDStats m_stats;
+	HUDConsumable m_conso;
 
 	public HUD(int x, int y, int w, int h, Player p) throws Exception {
 		m_x = x;
@@ -17,8 +18,9 @@ public class HUD {
 		m_width = w;
 		m_height = h;
 		m_player = p;
-		m_font = new HUDFont(0, 0, w/2, h, p);
-		m_stats = new HUDStats(m_font.m_width, 0, w - m_font.m_width, h, p);
+		m_font = new HUDFont(0, 0, w/3, h, p);
+		m_stats = new HUDStats(m_font.m_width, 0, w/3, h, p);
+		m_conso = new HUDConsumable(m_stats.m_x + m_stats.m_width, y, w/3, h, p);
 	}
 
 	public void tick(long elapsed) {
@@ -30,6 +32,7 @@ public class HUD {
 		Graphics gp = g.create(m_x, m_y, m_width, m_height);
 		m_font.paint(gp);
 		m_stats.paint(gp);
+		m_conso.paint(gp);
 		gp.dispose();
 	}
 
